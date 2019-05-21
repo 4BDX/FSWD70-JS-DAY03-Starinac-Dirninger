8ae639ebdd82a3f92e0fb2ac3a4f5efcbe9eab34
Max <maximilian@dirninger.com>
2019-05-21 13:17:24 +0200

    added styles...

--- basic4.js
+++ basic4.js
@@ -1,15 +1,19 @@
 var temperature = Math.floor((Math.random() * 50) - 5);
 if (temperature <= 5){
 
+
+      
+
 document.getElementById('text').innerHTML="the weather is cold";
-document.getElementById('cold').innerHTML="<img src='cold.jpg'>";
+document.getElementById('cold').innerHTML="<img width='100' height='100' src='cold.jpg'>";
 }
 
 else if (temperature>5 && temperature <= 20)
 
 {
 document.getElementById('text').innerHTML="the weather is moderate";
-document.getElementById('moderate').innerHTML="<img src='moderate.jpg'>";
+document.getElementById('moderate').innerHTML="<img id='imgeMod' width='600' height='200' src='moderate.jpg'>";
+document.getElementById('imgeMod').style.border = "3px solid red";
 
 }
 else 
@@ -17,3 +21,4 @@ else
 document.getElementById('text').innerHTML="the weather is hot";
 document.getElementById('hot').innerHTML="<img src='hot.jpg'>";
 }
+
